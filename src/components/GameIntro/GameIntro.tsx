b4057049c131ee57f0c4fe49f596adda878fb5ac
Timur Mironenko <timurtop2281337@gmail.com>
2023-03-11 20:50:21 +0200

    Created GameIntro component

---
+++ src/components/GameIntro/GameIntro.tsx
@@ -0,0 +1,16 @@
+import { useAppDispatch } from "../../app/hooks"
+import { startGame } from "../../features/gamePokemonSlice/gamePokemonSlice"
+import GameButton from "../GamePage/GameButton/GameButton"
+import styles from "./GameIntro.module.scss"
+
+
+export default function GameIntro() {
+    const dispatch = useAppDispatch()
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.header}>Game Info</h1>
+            <p className={styles.paragraph}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, mollitia libero. Tenetur fuga iure optio enim deleniti, cumque nihil necessitatibus ea quod minus aperiam sequi. Recusandae exercitationem iusto dolorum itaque?</p>
+            <GameButton onClick={() => dispatch(startGame())}>START GAME</GameButton>
+        </div>
+    )
+}
