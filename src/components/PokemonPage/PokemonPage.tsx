c9bce3bdb95beff88b72e46e7cbd9f964b5123ca
Timur Mironenko <timurtop2281337@gmail.com>
2023-10-15 22:34:24 +0300

    Updated IPokemon interface and added data fetch on load in PokemonPage

--- src/components/PokemonPage/PokemonPage.tsx
+++ src/components/PokemonPage/PokemonPage.tsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react"
 import styles from "./PokemonPage.module.scss"
 import { useParams } from "react-router-dom"
+import IPokemon from "../../types/IPokemon"
+import getPokemonImage from "../../functions/getPokemonImage"
 
 export const PokemonPage = () => {
     const { id } = useParams()
+    const [pokemon, setPokemon] = useState<IPokemon>()
+
+    useEffect(() => {
+        (async () => {
+            const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+            const pokemonData = await fetch(url).then(res => res.json())
+            setPokemon({
+                id: pokemonData.id,
+                name: pokemonData.name,
+                height: pokemonData.height,
+                weight: pokemonData.weight,
+                img: getPokemonImage(pokemonData),
+                types: pokemonData.types,
+                abilities: pokemonData.abilities,
+                stats: pokemonData.stats
+            })
+        })()
+    }, [])
+
     return (
         <div>
-            {id}
+            {pokemon && Object.values(pokemon).toLocaleString()}
         </div>
     )
 }
--- src/types/IPokemon.ts
+++ src/types/IPokemon.ts
@@ -1,5 +1,22 @@
 import PokemonType from "./PokemonType"
 
+interface Stat {
+    base_stat: number,
+    effort: number,
+    stat: {
+        name: string,
+        url: string
+    }
+}
+
+interface Ability {
+    ability: {
+        name: string,
+        url: string
+    },
+    is_hidden: boolean,
+    slot: number
+}
 
 export default interface IPokemon {
     id: number,
@@ -7,6 +24,8 @@ export default interface IPokemon {
     img: string,
     height: number,
     weight: number,
-    types: PokemonType[]
+    types: PokemonType[],
+    stats: Stat[],
+    abilities: Ability[]
 }
 
