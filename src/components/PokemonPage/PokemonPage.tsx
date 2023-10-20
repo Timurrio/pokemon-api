a448a9d8bfa344c752d6162ab1e396f090a63f7e
Timur Mironenko <timurtop2281337@gmail.com>
2023-10-20 21:40:11 +0300

    Added basic markup for PokemonPage component

--- src/components/PokemonPage/PokemonPage.tsx
+++ src/components/PokemonPage/PokemonPage.tsx
@@ -3,6 +3,8 @@ import styles from "./PokemonPage.module.scss"
 import { useParams } from "react-router-dom"
 import IPokemon from "../../types/IPokemon"
 import getPokemonImage from "../../functions/getPokemonImage"
+import loader from "../../assets/pokeballLoader.gif"
+import "../../styles/types.scss"
 
 function extractSprites(obj: any): string[] {
     delete obj["versions"]
@@ -43,9 +45,67 @@ export const PokemonPage = () => {
         })()
     }, [])
 
+    if (pokemon) {
+        return (
+            <div className={styles.page}>
+                <div className={styles.content}>
+                    <h1 className={styles.pokemon_name}>{pokemon.name}</h1>
+
+                    <div className={styles.main}>
+                        <div className={styles.main_left}>
+                            <img className={styles.main_image} src={pokemon.img} alt={pokemon.name} />
+
+                        </div>
+                        <div className={styles.main_right}>
+                            <h2>Height</h2>
+                            <p>{pokemon.height}</p>
+                            <h2>Weight</h2>
+                            <p>{pokemon.weight}</p>
+                            <div className={styles.types}>
+                                {
+                                    pokemon.types.map((type) => (
+                                        <span className={type.type.name}>{type.type.name}</span>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.stats}>
+                        <h2>Stats</h2>
+                        {
+                            pokemon.stats.map((stat) => (
+                                <div>Base {stat.stat.name}: {stat.base_stat}</div>
+                            ))
+                        }
+                    </div>
+
+                    <div className={styles.abilities}>
+                        <h2>Abilities</h2>
+                        {
+                            pokemon.abilities.map((ability) => (
+                                <span>{ability.ability.name}</span>
+                            ))
+                        }
+                    </div>
+
+                    <div className={styles.sprites}>
+                        <h2>Sprites</h2>
+                        {
+                            pokemon.sprites.map((sprite) => (
+                                <img src={sprite} alt={sprite} />
+                            ))
+                        }
+                    </div>
+
+
+                </div>
+            </div>
+        )
+    }
     return (
-        <div>
-            {pokemon && Object.values(pokemon).toLocaleString()}
+        <div className={styles.loader}>
+            <img src={loader} alt="loader" />
         </div>
     )
 }
