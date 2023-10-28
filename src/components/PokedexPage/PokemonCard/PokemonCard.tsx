1e0dd02f365ee8f5b3a34329eb6bffb9fcbbbbaf
Timur Mironenko <timurtop2281337@gmail.com>
2023-10-28 13:01:34 +0300

    Updated PokemonCard styles, added animations
    1. Added z-index to Navbar component
    2. Added animations to PokemonCard
    3. Updated PokemonCard styles

--- src/components/PokedexPage/PokemonCard/PokemonCard.tsx
+++ src/components/PokedexPage/PokemonCard/PokemonCard.tsx
@@ -9,15 +9,19 @@ const PokemonCard: React.FC<{ pokemon: Partial<IPokemon> }> = ({ pokemon }) => {
     return (
         <Link className={styles.link_wrapper} to={`/${pokemon.id}`}>
             <div key={pokemon.id} className={styles.card}>
-                <img src={pokemon.img} alt={pokemon.name} />
-                <p>{pokemon.id}</p>
-                <p>{pokemon.name}</p>
-                <div className={styles.types}>
-                    {
-                        pokemon.types && pokemon.types.map((type) => (
-                            <span className={`${type.type.name}`} key={type.slot}>{type.type.name}</span>
-                        ))
-                    }
+                <div className={styles.image_container}>
+                    <img className={styles.image} src={pokemon.img} alt={pokemon.name} />
+                </div>
+                <div className={styles.info_container}>
+                    <p className={styles.pokemon_id}>№{pokemon.id}</p>
+                    <p className={styles.pokemon_name}>{pokemon.name}</p>
+                    <div className={styles.pokemon_types}>
+                        {
+                            pokemon.types && pokemon.types.map((type) => (
+                                <span className={`${styles.pill} ${type.type.name}`} key={type.slot}>{type.type.name}</span>
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
         </Link>
