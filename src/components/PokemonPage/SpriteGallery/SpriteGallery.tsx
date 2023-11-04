debf5695024d33a37baadbfd15bca42cc39dbaa8
Timur Mironenko <timurtop2281337@gmail.com>
2023-11-04 20:13:14 +0200

    Updated SpriteGallery component
    Now SpriteGallery component displays only 5 preview slides simultaneously

--- src/components/PokemonPage/SpriteGallery/SpriteGallery.tsx
+++ src/components/PokemonPage/SpriteGallery/SpriteGallery.tsx
@@ -1,10 +1,20 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import { BsFillArrowLeftSquareFill, BsFillArrowRightSquareFill } from "react-icons/bs"
 import styles from "./SpriteGallery.module.scss"
 
 
 export const SpriteGallery = ({ sprites }: { sprites: string[] }) => {
     const [activeSlide, setActiveSlide] = useState(0)
+
+    const displayedSlides = useMemo(() => {
+        if (activeSlide - 2 >= 0 && activeSlide + 2 <= sprites.length - 1) {
+            return sprites.slice(activeSlide - 2, activeSlide + 3)
+        } else if (activeSlide - 2 < 0) {
+            return sprites.slice(0, 5)
+        } else {
+            return sprites.slice(sprites.length - 5, sprites.length)
+        }
+    }, [activeSlide])
 
     function handleRightArrow() {
         if (activeSlide < sprites.length - 1) {
@@ -33,7 +43,7 @@ export const SpriteGallery = ({ sprites }: { sprites: string[] }) => {
             <div className={styles.slides}>
                 {
                     sprites.map((sprite, index) => (
-                        <img onClick={() => { setActiveSlide(index) }} className={`${styles.slide} ${activeSlide === index ? styles["slide--active"] : ""}`} height={100} width={100} src={sprite} alt={sprite} />
+                        displayedSlides.includes(sprite) && <img onClick={() => { setActiveSlide(index) }} className={`${styles.slide} ${activeSlide === index ? styles["slide--active"] : ""}`} height={100} width={100} src={sprite} alt={sprite} />
                     ))
                 }
             </div>
