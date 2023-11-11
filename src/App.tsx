a3233df4ba7e214a0cbb681d1584be2beaa38623
Timur Mironenko <timurtop2281337@gmail.com>
2023-11-11 12:41:16 +0200

    Added ScrollToTopWrapper

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import GamePage from "./components/GamePage/GamePage";
 import PokedexPage from "./components/PokedexPage/PokedexPage";
 import Navbar from "./components/Navbar/Navbar";
 import { PokemonPage } from "./components/PokemonPage/PokemonPage";
+import ScrollToTopWrapper from "./components/ScrollToTopWrapper";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<PokedexPage />} />
-          <Route path="/:id" element={<PokemonPage />} />
-          <Route path="/game" element={<GamePage />} />
-        </Routes>
+        <ScrollToTopWrapper>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<PokedexPage />} />
+            <Route path="/:id" element={<PokemonPage />} />
+            <Route path="/game" element={<GamePage />} />
+          </Routes>
+        </ScrollToTopWrapper>
       </BrowserRouter>
     </div>
   );
